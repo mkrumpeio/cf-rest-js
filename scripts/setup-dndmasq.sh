ee2b0bd4c6a55e3d568da54cc627f5f6217b1aaf
Dost, Richard (GE Global Research) <dost@ge.com>
2017-05-15 15:03:28 -0700

    spring cleaning

--- scripts/setup-dndmasq.sh
+++
@@ -1,5 +0,0 @@
-apt-get update
-apt-get install dnsmasq httping
-echo "address=/bosh-lite.com/${CF_IP}" | tee -a /etc/dnsmasq.conf > /dev/null
-dpkg-reconfigure resolvconf
-/etc/init.d/dnsmasq restart
